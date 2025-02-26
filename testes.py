1bfbac9ab80009fedd21885312e5481e8518e369
max <maximiliano01253@gmail.com>
2025-02-26 11:30:22 -0300

    gitignore pra ignorar o arquivo testes.py

--- testes.py
+++
@@ -1,9 +0,0 @@
-a= pygame.time.get_ticks()
-frase='abrobinha'
-#dentro do loopp
-if a-pygame.time.get_ticks():
-    frase=''
-#metodo click
-if botao foi clicado:
-    frase=random.choice([lista de frases])
-    a=pygame.time.get_ticks()
